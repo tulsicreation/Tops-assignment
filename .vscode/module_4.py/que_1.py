a603a31bfb1ab52b93fd83888f43e60960908d84
tulsicreation <nehaketansurani@gmail.com>
2024-01-08 12:32:24 +0530

    second commit

---
+++ .vscode/module_4.py/que_1.py
@@ -0,0 +1,11 @@
+# Q-1: What is File function in python? What is keywords to create and write file.
+
+# ans:
+# File handling is an important part of any web application.
+# Python has several functions for creating, reading, updating, and deleting files.
+
+# w – stands for Write – This keyword opens the file for writing.
+# It can create a non-existing file.
+
+# x – stands for Create – This keyword is used to create the respective file.
+# If the file is already in existence, then it will return an error.
